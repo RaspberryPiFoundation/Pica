1e3b47abed0d897d8eb3b4b982bba03149eca110
Jonic Linley <jonic@100yen.co.uk>
2017-04-06 17:06:13 +0100

    Added public directory for generated assets

---
+++ public/assets/javascripts/pica.js
@@ -0,0 +1,10 @@
+/**
+ * pica.js
+ * 
+ * Pica - The Raspberry Pi Website Patten Library
+ * @version v0.0.0
+ * @link https://pica.raspberrypi.org/
+ */
+
+//  Import Modules from component JavaScript files
+//  import * as another from 'components/foo';
